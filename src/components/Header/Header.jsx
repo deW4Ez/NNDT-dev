d60ff2dfd9152e4792d1332f1c59dbe0ebd2c2fc
deW4Ez <82871591+deW4Ez@users.noreply.github.com>
2022-10-08 14:07:41 +0500

    Add main files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="heading">
+            <div>
+                <h1>Neural Network for Digital Twin (NNDT)</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
